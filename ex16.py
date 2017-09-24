3ae165edcc4d1c0b2d27e5d58280bfd17422e73c
Mike Kery <dev.Kery.Mike@dev.bwater.com>
2017-09-24 18:15:19 -0400

    Committing exercise 16

--- ex16.py
+++ ex16.py
@@ -3,7 +3,7 @@ from sys import argv
 script, filename = argv
 
 print "We're going to erase %r." % filename
-print "If you don;t want that, hit CTRL-C (^C)."
+print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
 
 raw_input("?")
